b4558e283a666046865344206895fc7de82281d8
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2015-12-06 22:54:29 +0100

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "School Management",
+    "version" : "0.1",
+    "category" : "Accounting",
+    "sequence": 60,
+    "complexity" : "normal",
+    "author" : "ColourCog.com",
+    "website" : "http://colourcog.com",
+    "depends" : [
+        "base",
+        "sale",
+        "account",
+    ],
+    "summary" : "Modifies standard OpenERP functionality",
+    "description" : """
+School Management
+========================
+This module offers tools to manage primary and secondary schools
+
+Features:
+-------------------------------
+* Student database
+* Registration process
+    """,
+    "data" : [
+    ],
+    "application": False,
+    "installable": True
+}
+
